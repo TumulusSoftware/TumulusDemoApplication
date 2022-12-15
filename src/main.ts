b68fc0ea6fbf4b68c561476002c6d14ac80eee56
rolia-net <ocean.tiong@gmail.com>
2022-12-15 10:47:14 -0600

    Work in progress

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router';
+import router from '@/router';
 import { useRouter } from 'vue-router';
 
 /* Core CSS required for Ionic components to work properly */
